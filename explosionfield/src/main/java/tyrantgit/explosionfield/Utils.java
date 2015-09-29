a90766befad69b97d5c7b6581c2d9f9c03056b78
JohnTsai <johntsai.work@gmail.com>
2015-09-29 19:50:17 +0800

    update

--- explosionfield/src/main/java/tyrantgit/explosionfield/Utils.java
+++ explosionfield/src/main/java/tyrantgit/explosionfield/Utils.java
@@ -29,13 +29,26 @@ public class Utils {
     private Utils() {
     }
 
+    /**
+     * 在DisplayMetrics类中属性density的值为dpi/160，可用于px与dip的互相转换。
+     */
     private static final float DENSITY = Resources.getSystem().getDisplayMetrics().density;
     private static final Canvas sCanvas = new Canvas();
 
+    /**
+     * dp转px px = dp * (density)
+     * @param dp
+     * @return px
+     */
     public static int dp2Px(int dp) {
         return Math.round(dp * DENSITY);
     }
 
+    /**
+     * 由View对象返回Bitmap对象（ImageView和其他View分别处理）
+     * @param view
+     * @return
+     */
     public static Bitmap createBitmapFromView(View view) {
         if (view instanceof ImageView) {
             Drawable drawable = ((ImageView) view).getDrawable();
@@ -57,6 +70,15 @@ public class Utils {
         return bitmap;
     }
 
+    /**
+     * 使用Bitmap的createBitmap方法创建bitmap对象
+     * 若遇到OOM,调用gc()方法,然后递归调用自己retryCount次直到返回Bitmap，否则返回null
+     * @param width
+     * @param height
+     * @param config
+     * @param retryCount
+     * @return
+     */
     public static Bitmap createBitmapSafely(int width, int height, Bitmap.Config config, int retryCount) {
         try {
             return Bitmap.createBitmap(width, height, config);
